e5b4c8e1bb514682ca138d0e57af1a9c99581ba7
Digo Ribeiro <rbarbosaribeiro@gmail.com>
2021-06-01 19:48:36 -0300

    resolvendo exercicios do dia 25/05

--- tablet/js/javascript.js
+++ tablet/js/javascript.js
@@ -1,13 +1,22 @@
 // alert("Olá mundo");
 const imagemBloqueada = document.getElementById("imagemBloqueada");
 const imagemDesbloqueada = document.getElementById("imagemDesbloqueada");
+const botaoBloquear = document.getElementById("botaoBloquear");
+const botaoDesbloquear = document.getElementById("botaoDesbloquear");
+const titulo = document.getElementById("titulo");
 
-document.getElementById("botaoDesbloquear").addEventListener("click", function(){
+botaoDesbloquear.addEventListener("click", function(){
     imagemBloqueada.setAttribute("class", "esconder"); 
     imagemDesbloqueada.setAttribute("class", "mostrar");
+    botaoDesbloquear.setAttribute("disabled", true);
+    botaoBloquear.removeAttribute("disabled");
+    titulo.innerHTML = "Projeto Tablet (Desloqueado)";
 });
 
-document.getElementById("botaoBloquear").addEventListener("click", function(){
+botaoBloquear.addEventListener("click", function(){
     imagemBloqueada.setAttribute("class", "mostrar");
     imagemDesbloqueada.setAttribute("class", "esconder");
+    botaoDesbloquear.removeAttribute("disabled");
+    botaoBloquear.setAttribute("disabled", true);
+    titulo.innerHTML = "Projeto Tablet (Bloqueado)";
 });
