afbc04c3d00709ef206a0f7e69d9e31af952f283
Digo Ribeiro <rbarbosaribeiro@gmail.com>
2021-06-08 20:30:47 -0300

    Aula de lógica parte 1

---
+++ logica-programacao/js/logica.js
@@ -0,0 +1,65 @@
+// boleano || Verdadeiro ou falso
+
+// 0 = False // False
+// 1 = True // Verdadeiro
+
+// Se o valor é 'não' == Verdadeiro || Se valor for diferente de 'não' == falso
+
+// exemplos de verficação
+
+/*
+
+** Verificando se é verdadeiro ou falso
+const nao = 'não';
+
+if(nao == 'não') alert('É verdeiro');
+
+if(nao == 'nao') alert('É verdeiro');
+
+
+** Verificando valor e tipo
+const numero = 100;
+
+if(numero == "100") alert("chamou o alerta"); verificação simples = verificar se o valor é igual
+
+if(numero === "100") alert("chamou o alerta"); verificação complexa = verifica se o valor e o tipo são iguais
+*/
+
+// Variaveis
+var valor = 'ValorDeVariavel'; // Efeito global !*** Não usar*** !
+const valorConst = 'ValorVariavel'; // Não pode mudar o valor na execução do programa
+let valorLet = 'ValorLet'; // Pode mudar o valor na execução do programa
+
+// O que posso guardar em variaveis
+const verdadeiroOuFalso = false;
+const numeros = 123321;
+const float = 1.2;
+// const funcaoAlerta = alert();
+
+// Operações aritimeticas
+
+// + soma
+// - subtração
+// * mutiplição
+// / divisão
+// ** exponenciação
+// % modulo (resto da divisão)
+// == ou === || 1 + 1 == 2 ou 1 + 1 === 2 saber se o resultado é verdadeiro.
+// = atribição de valor
+
+// ++ incremento
+// -- decremento
+
+// !== Diferente
+// < Menor que
+// > Maior que
+// <= Menor ou igual
+// >= Maior ou igual
+// ? Valor ternario ex: numeroUm >= numeroDois ? 'Sim menor ou igual' : 'Não é menor ou igual';
+
+// E &&
+// OU ||
+// ! Negação
+
+// null quando não tem nada ou nulo
+// undefined quando o valor não foi definido
